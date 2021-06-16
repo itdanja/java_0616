31e696d40bc1f4779b4d470ec47f691a136549a0
Sera Lee <issell@naver.com>
2021-06-16 21:41:40 +0900

    Day01

---
+++ java1/src/Day01/Day01_1.java
@@ -0,0 +1,43 @@
+package Day01;
+// package 패키지명;
+	// 주석 : 설명달기 [ 코드 실행에 관련없는 줄 ] 
+public class Day01_1 { // class 시작 
+
+	// 자동완성 단축키 : ctrl+스페이바
+	// main => ctrl+스페이바 => 엔터
+	
+	public static void main(String[] args) { // {  } : main 메소드의 시작{ 과 끝 }
+		
+				// main 메소드[=함수] : main 스레드를 가지고 있음
+					// 스레드 : 프로세스내 코드를 읽어주는 역할
+						// main 없으면 코드실행 x
+							// 모든 코드의 시작지점은 main 메소드부터 시작 
+	// 클래스 , 메소드 : 시작{   끝}
+		// 1. 출력 : syso + ctrl +스페이스바
+			// println("출력내용");  // 출력후 자동 줄바꿈 
+			// print("출력내용"); // 출력후 자동 줄바꿈X
+		
+		System.out.println("안녕하세요");
+			// System : 시스템[컴퓨터] 관련된 코드를 제공해주는 클래스  
+				// . : 클래스내 멤버[데이터]에 접근 연산자
+					// out : 인터페이스 
+						// println( ) : 출력 함수 
+							// ; : 일처리 [ 모든 코드 끝에 ; ]
+		System.out.println("java 첫 프로그래밍");
+		System.out.println("화면 출력중 ");
+		System.out.println("프로그래밍중");
+		
+		System.out.println("java 첫 프로그래밍");
+		System.out.print("화면 출력중 ");
+		System.out.println("프로그래밍중");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	} // main 끝 
+} // class 끝 
---
+++ java1/src/Day01/Day01_2.java
@@ -0,0 +1,71 @@
+package Day01;
+
+public class Day01_2 { // c s
+	
+	
+	public static void main(String[] args) { // m s
+		
+		
+		// 출력 
+		System.out.println(10); // 숫자는  " " 처리 x 
+		System.out.println(true); // 논리 " " 처리 x 
+		System.out.println("java"); 
+			// 키워드[ 미리 만들어진 단어 ]을 제외한 단어들은 " " 처리 O
+		
+		// 제어문자  => 원화기호 \
+		System.out.print("안녕\njava\n입니다\n");
+		System.out.print("안녕\tjava\t입니다\t");
+		System.out.print("안녕\tjava\r입니다\n");
+			// 제어문자 
+				// 1. \n : 줄바꿈
+				// 2. \t : 들여쓰기[tab]
+				// 3. \r : 캐리지리턴 [ 맨 앞으로 이동 ] 
+		// 형식문자 
+		System.out.printf( "현재 %d 입니다\n" , 10  );
+			System.out.println("현재 " + 10 + " 입니다");
+							// + 연결 연산자 [ 문자열 + 문자열 // 숫자 + 문자열 ]
+							// + 더하기 연산자 [ 숫자 + 숫자 ]
+		System.out.printf( "현재 %s 입니다\n" , "java");
+		System.out.printf( "현재 %f 입니다\n" , 10.5);
+		System.out.printf( "현재 %c 입니다\n" , 'A' );
+			// 형식문자 [ 형식 format => 모양 꾸미기 ]
+				// 1. %d : 정수
+				// 2. %f : 실수 [ 소수점 ]
+				// 3. %s : 문자열
+				// 4. %c : 문자
+	
+		// 예제1 ) 출석부 출력 [ print ] 
+		System.out.print("================출석부===============\n");
+		System.out.print("이름\t1교시\t2교시\t3교시\t비고\n");
+		System.out.print("유재석\t출석\t결석\t출석\t공란\n");
+		System.out.print("강호동\t출석\t결석\t결석\t공란\n");
+		System.out.print("===================================\n");
+		
+		// 문제1 ) 예제1과 동일하게 출력 [ printf ]
+		System.out.printf("================%s===============\n" , "출석부");
+		System.out.printf("%s\t%s\t%s\t%s\t%s\n" , "이름" , "1교시" , "2교시" , "3교시" , "비고");
+		System.out.printf("%s\t%s\t%s\t%s\t%s\n" , "유재석" , "출석" , "결석" , "출석" , "공란");
+		System.out.printf("%s\t%s\t%s\t%s\t%s\n" , "강호동" , "출석" , "결석" , "결석" , "공란");
+		System.out.printf("===================================\n");
+		
+		// 문제2 ) ★ 출력
+					★
+		           ★★★
+				  ★★★★★
+				 ★★★★★★★ 
+			   ★★★★★★★★★★★
+		   ★★★★★★★★★★★★★★★★★★
+			  ★★★★★★★★★★★★★
+			    ★★★★★★★★★
+			     ★★★★★★★
+			     ★★★ ★★★
+		        ★★★   ★★★
+		       ★★		★★
+		      ★		   	  ★
+		      
+		      
+		
+	} // m e
+	
+
+}// c e 

c03c98c777fc0b47c5be64ef240abac187cf92ae
itdanja <itdanja@kakao.com>
2021-07-07 21:47:42 +0900

    day07

--- java1/src/Day07/Day07_3.java
+++ java1/src/Day07/Day07_3.java
@@ -12,7 +12,8 @@ public class Day07_3 {
 	// 리스트 선언 : <클래스>  <> 안에 들어가는 객체를 여러개 저장할수있는 게시물등록 리스트 
 	public static List<게시물> 게시물목록 = new ArrayList<>();
 		// 리스트명.add( 객체명 ) : 해당객체를 리스트에 추가 
-	
+		// 리스트명.get( 인덱스번호 ) : 해당 인덱스 번호의 객체 호출 
+			// 인덱스 : 리스트에[ 저장된 순서  = 0번시작 ] 
 	public static void main(String[] args) {
 		
 		while(true ) {
@@ -27,7 +28,9 @@ public class Day07_3 {
 				게시물.게시물등록();
 			}
 			if( 선택 == 2 ) { // 게시물의 번호를 입력받아 해당 게시물의 모든 정보를 출력 
-				
+				System.out.println("---> 게시물 번호 선택 : ");
+				int 게시물번호 = scanner.nextInt();
+				게시물.게시물상세보기(게시물번호);
 			}
 		
 			
--- java1/src/Day07/게시물.java
+++ java1/src/Day07/게시물.java
@@ -41,13 +41,31 @@ public class 게시물{
 		// 게시물 목록 
 		// for( 클래스명 임시객체명 : 리스트명 ) : 리스트내 객체를 임시객체에 하나씩 대입 
 		System.out.println("게시물번호 \t 게시물제목 \t\t 작성자 \t 작성일 \t\t 조회수 ");
-		int i = 0 ;
+		
+		int i = 0 ; // 게시물 개수 세기 
+		
 		for( 게시물 게시물 : Day07_3.게시물목록 ) {
 			System.out.println( i +" \t\t "+게시물.제목+" \t\t "+게시물.작성자+"\t"
 							+게시물.작성일+" \t "+게시물.조회수);
-			i++; 
+			i++; // 게시물 당 하나씩 1씩 증가 
 		}
-
+	}
+	
+	public static void 게시물상세보기( int 게시물번호) {
+		System.out.println(" [ 게시물 상세페이지 ] ");
+		// 게시물번호[인덱스]를 인수로 받는 메소드 
+			// 상세보기 했을때 조회수 증가 
+			Day07_3.게시물목록.get(게시물번호).조회수 ++ ;
+		
+		// 선택 게시물 가져오기 
+		게시물 temp = Day07_3.게시물목록.get(게시물번호);
+		// 게시물 출력 
+		System.out.println(" 제목 : " + temp.제목);
+		System.out.println(" 작성자 : " + temp.작성자 +
+				" 작성일 : " + temp.작성일 +
+				" 조회수 : " + temp.작성자);
+		
+		System.out.println(" 내용 : " + temp.내용);
 	}
 	
 	

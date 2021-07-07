46297b0b7f8f3533767398e5b9977af92540b5c9
itdanja <itdanja@kakao.com>
2021-07-07 21:34:08 +0900

    day07

---
+++ java1/src/Day07/Day07_3.java
@@ -0,0 +1,39 @@
+package Day07;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day07_3 {
+	
+	// main 밖에 작성하는 이유 : main 메소드 외에서도 사용되는 경우 [ 다른 클래스도 사용가능 ] 
+	public static Scanner scanner = new Scanner(System.in);
+	
+	// 리스트 선언 : <클래스>  <> 안에 들어가는 객체를 여러개 저장할수있는 게시물등록 리스트 
+	public static List<게시물> 게시물목록 = new ArrayList<>();
+		// 리스트명.add( 객체명 ) : 해당객체를 리스트에 추가 
+	
+	public static void main(String[] args) {
+		
+		while(true ) {
+			
+			System.out.println(" -----------------------> 커뮤니티 --------------------> ");
+				
+				게시물.게시물출력();	
+				
+			System.out.println("1.등록 2.상세보기");
+			int 선택 = scanner.nextInt();
+			if( 선택 == 1 ) {
+				게시물.게시물등록();
+			}
+			if( 선택 == 2 ) { // 게시물의 번호를 입력받아 해당 게시물의 모든 정보를 출력 
+				
+			}
+		
+			
+		}
+		
+		
+	}
+
+}
---
+++ java1/src/Day07/게시물.java
@@ -0,0 +1,66 @@
+package Day07;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class 게시물{
+
+	// 1. 필드 
+	String 제목 ; 
+	String 내용 ;
+	String 작성자 ; 
+	String 작성일 ;
+	int 조회수 ;
+	// 2. 생성자 [ 게시물 작성시 초기값 ] 
+	public 게시물( String 제목, String 내용 , String 작성자 ) {
+		this.제목 = 제목;
+		this.내용 = 내용;
+		this.작성자 = 작성자;
+			Date date = new Date();
+			SimpleDateFormat format = new SimpleDateFormat("yy-MM-dd");
+		this.작성일 = format.format(date);
+		this.조회수 = 0;
+	}
+	// 3. 메소드 
+	public static void 게시물등록 ( ) {
+		System.out.println(" [게시물 등록 페이지]");
+			// .nextLine(); 두번 이상 사용시 문제 발생 
+			Day07_3.scanner.nextLine(); // 문제 처리 방법
+		System.out.print(" 제목 : ");	String 제목 = Day07_3.scanner.nextLine();
+			// .next() : 공백 입력 불가 
+			// .nextLine() : 공백 포함 입력 가능 
+		System.out.print(" 내용 : ");	String 내용 = Day07_3.scanner.nextLine();
+		System.out.print(" 작성자 : ");	String 작성자 = Day07_3.scanner.nextLine();
+		
+		게시물 temp = new 게시물(제목, 내용, 작성자);
+		System.out.println(" [완료] 게시물이 등록 되었습니다 ");
+		// 게시물을 배열 혹은 리스트에 저장 
+		Day07_3.게시물목록.add(temp);
+	}
+	public static void 게시물출력() {
+		// 게시물 목록 
+		// for( 클래스명 임시객체명 : 리스트명 ) : 리스트내 객체를 임시객체에 하나씩 대입 
+		System.out.println("게시물번호 \t 게시물제목 \t\t 작성자 \t 작성일 \t\t 조회수 ");
+		int i = 0 ;
+		for( 게시물 게시물 : Day07_3.게시물목록 ) {
+			System.out.println( i +" \t\t "+게시물.제목+" \t\t "+게시물.작성자+"\t"
+							+게시물.작성일+" \t "+게시물.조회수);
+			i++; 
+		}
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}

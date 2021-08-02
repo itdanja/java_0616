6f61a31222a59df1ca621414babe82ab96025f1f
itdanja <itdanja@kakao.com>
2021-08-02 21:01:20 +0900

    day05

---
+++ java2/src/day05/Day05_5_메일전송.java
@@ -0,0 +1,20 @@
+package day05;
+
+public class Day05_5_메일전송 {
+
+	
+	public static void main(String[] args) {
+		
+		// 외부 API : 기본적으로 제공 받지 않는 라이브러리 다운로드 받아 사용하기 
+		
+		// SMTP : Simple Mail Transfer Protocol 
+			// Protocol : 통신 규약 [ 통신할때 지켜야되는 규약(약속) ]
+		
+		// 1. STMP 클래스를 사용하기 위한 라이브러리 다운로드 
+		
+		
+		
+	}
+	
+	
+}

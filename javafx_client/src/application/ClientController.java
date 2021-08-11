a877b926d379229087105cb8586370f0ef1be157
itdanja <itdanja@kakao.com>
2021-08-11 21:20:39 +0900

    day19

--- javafx_client/src/application/ClientController.java
+++ javafx_client/src/application/ClientController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,14 +18,62 @@ public class ClientController implements Initializable {
 
 	// 1. 클라이언트 소켓 
 	Socket socket;
+	
 	// 2. 클라이언트 시작 메소드
-	public void clientstart() {}
+	public void clientstart() {
+		// 1. 소켓생성 --> 2.서버소켓으로 부터 연결요청 
+			
+		Thread thread = new Thread() { // 멀티스레드 
+			@Override // 재정의
+			public void run() {
+				try {
+					socket = new Socket("127.0.0.1" , 1234); // 서버소켓의 ip와 post 
+					receive();
+				}catch (Exception e) {}
+			}
+			
+		}; // 스레드 괄호 end 
+		thread.start(); // run메소드(멀티스레드 로직 ) <---- start( run메소드호출 ) 
+		
+	}
 	// 3. 클라이언트 종료 메소드 
-	public void clientstop() {}
+	public void clientstop() {
+		try {
+			socket.close(); // 소켓 닫기 
+		}catch (Exception e) {}
+	}
+	
 	// 4. 메시지 보내는 메소드 
-	public void send( String msg ) {}
+	public void send( String msg ) {
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					OutputStream outputStream = socket.getOutputStream();
+					byte[] bytes = msg.getBytes(); // 문자열 -> 바이트 
+					outputStream.write(bytes); // 해당 바이트 내보내기 
+					outputStream.flush(); // 스트림 초기화
+				}catch (Exception e) {}
+			}
+		}; // 쓰레드 끝 
+		thread.start();
+	}
+	
 	// 5. 메시지 받는 메소드 
-	public void receive() {}
+	public void receive() {
+		while(true) {
+			try {
+				InputStream inputStream = socket.getInputStream();
+				byte[] bytes = new byte[1000];
+				inputStream.read(bytes); // 바이트 가져와서 
+				String msg  = new String(bytes); // 바이트 => 문자열 
+				// 받은 메시지를 메시지창에 띄우기 
+					//Platform.runLater( () -> {실행코드} );
+				Platform.runLater( () -> { txtcontents.appendText(msg); } );
+			
+			}catch (Exception e) {}
+		}
+	}
 	
 	
 	@Override
@@ -52,7 +102,7 @@ public class ClientController implements Initializable {
     void connect(ActionEvent event) {
     	if( btnconnect.getText().equals("접속") ) {
     		//1. 클라이언트 실행 
-    			// 미구현
+    			clientstart();
     		Platform.runLater( () -> txtcontents.appendText("[채팅방 접속]\n"));
     		// 2. 접속후 
     		btnconnect.setText("종료");
@@ -62,7 +112,7 @@ public class ClientController implements Initializable {
     	}else {
     		
     		// 1.클라이언트 종료
-    			// 미구현
+    			clientstop();
     		Platform.runLater( () -> txtcontents.appendText("[채팅방 퇴장]\n"));
     		// 2. 종료후 
     		btnconnect.setText("접속");
@@ -74,8 +124,39 @@ public class ClientController implements Initializable {
     @FXML
     void send(ActionEvent event) {
 
+    	// 메시지 보내기 
+    	send( txtname.getText() + " : " + txtmsg.getText()+"\n" );
+    	
+    	// 보내기후 
+    	txtmsg.setText(""); // 입력창 초기화
+    	txtmsg.requestFocus(); // 포커스 : 커서
+    	
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 	
 	
 	
